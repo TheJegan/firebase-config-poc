e72b25cf227aaa3b419f62f2b8926b9c66207fb7
Jegan Matthews <jematthe@jematthe011579.starbucks.net>
2018-09-19 15:26:00 -0700

    Initial config

---
+++ config.js
@@ -0,0 +1,32 @@
+{
+  "parameters":[
+    {
+      "key":"welcome_message",
+      "value_options":[
+        {
+          "value":"Welcome to this sample app"
+        }
+      ]
+    },
+    {
+      "key":"welcome_message_caps",
+      "value_options":[
+        {
+          "value":"false"
+        }
+      ]
+    }
+  ],
+  "version":{
+    "version_number": "1",
+    "update_time":"2018-05-11T18:46:40Z",
+    "update_user":{
+      "name":"Jegan Matthews",
+      "email":"jematthe@starbucks.com",
+      "imageUrl":"http://image.google.com/path-to-profile-photo-for-jane"
+    },
+    "description":"Adding welcome messages",
+    "origin":"CONSOLE",
+    "update_type":"INCREMENTAL_UPDATE"
+  }
+}
