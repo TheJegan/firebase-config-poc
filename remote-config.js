c0c1392cebf997011acf0414699f01eea8a5b5e8
no_indent <matthewsjegan@gmail.com>
2018-09-28 18:14:36 -0700

    Update remote-config.js

--- remote-config.js
+++ remote-config.js
@@ -25,7 +25,7 @@
       "email":"jane@developer.org",
       "imageUrl":"http://image.google.com/path-to-profile-photo-for-jane"
     },
-    "description":"Adding welcome messages",
+    "description":"Adding welcome messages. Back back back",
     "origin":"CONSOLE",
     "update_type":"INCREMENTAL_UPDATE"
   }
