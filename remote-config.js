d14a955ecf0d933c2d4444ef877ec35a9e794e21
no_indent <matthewsjegan@gmail.com>
2018-10-05 11:57:22 -0700

    Update remote-config.js

--- remote-config.js
+++ remote-config.js
@@ -13,7 +13,7 @@
       "key":"favorite_coffee",
       "value_options":[
         {
-          "value":"Welcome to this sample app again."
+          "value":"Welcome to this application"
         }
       ]
     },
